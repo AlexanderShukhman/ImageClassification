2278ac513e4b531fb4277f325045371f7b85580e
AlexanderShukhman <60272765+AlexanderShukhman@users.noreply.github.com>
2025-05-05 17:54:19 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import io
 import streamlit as st
 from PIL import Image
 import numpy as np
-from tensorflow.keras.applications import EfficientNetB0
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
+from keras.api.applications import EfficientNetB0
+from keras.api.preprocessing import image
+from keras.api.applications.efficientnet import preprocess_input, decode_predictions
 
 
 @st.cache_resource
